5cc2cb377d5f825bb971c705c5725646eb1f76a9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-03-12 23:39:55 -0300

    ..

--- apps/meteor/tests/e2e/omnichannel/omnichannel-livechat.spec.ts
+++ apps/meteor/tests/e2e/omnichannel/omnichannel-livechat.spec.ts
@@ -13,7 +13,7 @@ const newUser = {
 test.use({ storageState: Users.user1.state });
 
 test.describe.serial('OC - Livechat', () => {
-	let poLiveChat: OmnichannelLiveChat | undefined;
+	let poLiveChat: OmnichannelLiveChat;
 	let poHomeOmnichannel: HomeOmnichannel;
 
 	test.beforeAll(async ({ api }) => {
